41816029a64329d958c5cf2b968d4bad5bf2b25c
Yuunan Kin <yuunan.kin@gmail.com>
2021-05-16 11:20:54 +0900

    fix some names

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          React Starter
+          Node Advanced
         </Link>
         <button
           className="navbar-toggler"
